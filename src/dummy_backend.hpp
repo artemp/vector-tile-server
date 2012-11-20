5b18d9ec7027c2b4b164af42c46773ed8bdc172f
artemp <artem@mapnik.org>
2012-11-20 13:46:54 +0000

    + support multi geometries + cleanups

--- src/dummy_backend.hpp
+++ src/dummy_backend.hpp
@@ -23,9 +23,7 @@
 #ifndef MAPNIK_DUMMY_BACKEND_HPP
 #define MAPNIK_DUMMY_BACKEND_HPP
 
-// mapnik
-//#include <mapnik/util/geometry_to_wkb.hpp>
-// proto
+// protobuf io
 #include <google/protobuf/io/coded_stream.h>
 #include <google/protobuf/io/zero_copy_stream_impl_lite.h>
 //boost
@@ -40,18 +38,21 @@ namespace mapnik
 typedef std::pair<uint32_t,uint32_t> coord_type;
 typedef std::vector<coord_type> path_type;
 typedef std::vector<uint32_t> tags_type;
+typedef std::vector<uint32_t> index_type;
 
 struct tile_element
 {
     eGeomType type;
     tags_type tags;
     path_type path;
+    index_type index;
 };
 
 struct element_writer
 {
     google::protobuf::io::StringOutputStream  raw_output_;
     google::protobuf::io::CodedOutputStream   coded_output_;
+
     explicit element_writer(std::string & buffer)
         : raw_output_(&buffer),
           coded_output_(&raw_output_)
@@ -87,20 +88,13 @@ private:
     std::vector<std::string> custom_values_;
     boost::ptr_vector<tile_element> tile_elements_;
     std::auto_ptr<tile_element> element_;
-    google::protobuf::io::ZeroCopyOutputStream * raw_output_;
-    google::protobuf::io::CodedOutputStream * coded_output_;
-
+    google::protobuf::io::StringOutputStream raw_output_;
+    google::protobuf::io::CodedOutputStream coded_output_;
 public:
-    explicit dummy_backend(std::string & output):
-        raw_output_(new google::protobuf::io::StringOutputStream(&output)),
-        coded_output_(new google::protobuf::io::CodedOutputStream(raw_output_))
+    explicit dummy_backend(std::string & output)
+        : raw_output_(&output),
+          coded_output_(&raw_output_)
     {}
-
-    ~dummy_backend()
-    {
-        delete coded_output_;
-        delete raw_output_;
-    }
 
     boost::ptr_vector<tile_element> const& tile_elements() const
     {
@@ -112,19 +106,11 @@ public:
     }
 
     template <typename T>
-    void start_tile_element( T & feature)
+    void start_tile_element( T & feature, eGeomType type)
     {
-        // get geometry type FIXME: pass as an argument
-        eGeomType type = Unknown;
-        BOOST_FOREACH(geometry_type const& g, feature.paths())
-        {
-            type = g.type();
-            break;
-        }
         /////////////////////////////////////////////////////
-
         element_.reset(new tile_element);
-        element_->type = type;
+        element_->type = type; // geometry type POLY/LINE/POINT
 
         feature_kv_iterator itr = feature.begin();
         feature_kv_iterator end = feature.end();
@@ -137,7 +123,7 @@ public:
                 boost::optional<tags::tag_type> tag = tags::tag_value_from_name(name,val.to_string());
                 if ( tag )
                 {
-                    std::cout << "TAG = " << tag << std::endl;
+                    //std::cout << "TAG = " << tag << std::endl;
                     if (*tag < TAGS_MAX)
                     {
                         element_->tags.push_back(*tag);
@@ -150,9 +136,9 @@ public:
                             tags_.push_back(*tag_key);
                             custom_values_.push_back(val.to_string());
                             element_->tags.push_back((1023 + tags_.size()));
-                            std::cout << "CUSTOM TAG=" <<*tag << " "
-                                      << name <<  "="  << val.to_string()
-                                      << " tag_key=" << (tags_.size() + 1023) << std::endl;
+                            //std::cout << "CUSTOM TAG=" <<*tag << " "
+                            //<< name <<  "="  << val.to_string()
+                            //        << " tag_key=" << (tags_.size() + 1023) << std::endl;
                         }
                     }
                 }
@@ -175,6 +161,7 @@ public:
 
         if (element_.get())
         {
+            uint32_t start = element_->path.size();
             while ((vtx.cmd = path.vertex(&vtx.x, &vtx.y)) != SEG_END)
             {
                 int32_t cur_x = static_cast<int32_t>(vtx.x * SCALE);
@@ -200,7 +187,9 @@ public:
                 y = cur_y;
                 ++count;
             }
-            std::cout << "count = " << count << std::endl;
+            uint32_t size = element_->path.size() - start;
+            element_->index.push_back(size);
+            //std::cout << "count = " << count << std::endl;
         }
     }
 
@@ -208,9 +197,9 @@ public:
     {
         // tile tags
         // num tags
-        coded_output_->WriteTag(0x08);
-        coded_output_->WriteVarint32(tags_.size());
-        coded_output_->WriteTag(0x12);
+        coded_output_.WriteTag(0x08);
+        coded_output_.WriteVarint32(tags_.size());
+        coded_output_.WriteTag(0x12);
 
         std::string buffer;
         element_writer writer(buffer);
@@ -218,37 +207,34 @@ public:
         BOOST_FOREACH(tags::tag_type tag, tags_)
         {
             writer.write_varint32(tag);
-            std::cerr << tag << ",";
+            //std::cerr << tag << ",";
         }
         std::size_t bytes = writer.bytes_written();
 
-        coded_output_->WriteVarint32(bytes);
-        coded_output_->WriteRaw(buffer.data(), bytes);
+        coded_output_.WriteVarint32(bytes);
+        coded_output_.WriteRaw(buffer.data(), bytes);
 
-        std::cerr << "TILE TAGS VALUES [";
+        //std::cerr << "TILE TAGS VALUES [";
 
         BOOST_FOREACH(std::string const& val, custom_values_)
         {
-            coded_output_->WriteTag(0x1a);
-            coded_output_->WriteVarint32(val.size());
-            coded_output_->WriteRaw(val.data(),val.size());
-            std::cerr << '\"' << val << "\",";
+            coded_output_.WriteTag(0x1a);
+            coded_output_.WriteVarint32(val.size());
+            coded_output_.WriteRaw(val.data(),val.size());
+            //std::cerr << '\"' << val << "\",";
         }
-        std::cerr << "]" << std::endl;
-
-
+        //std::cerr << "]" << std::endl;
 
         BOOST_FOREACH(tile_element const& elem, tile_elements_)
         {
             if (elem.path.empty())
                 continue;
-
             if (elem.type == Polygon)
-                coded_output_->WriteTag(0x62);
+                coded_output_.WriteTag(0x62);
             else if (elem.type == LineString)
-                coded_output_->WriteTag(0x5a);
+                coded_output_.WriteTag(0x5a);
             else if (elem.type == Point)
-                coded_output_->WriteTag(0x6a);
+                coded_output_.WriteTag(0x6a);
 
             if (elem.tags.size() > 0)
             {
@@ -262,26 +248,29 @@ public:
                     element_writer writer(tags_buffer);
                     writer.write_tag(0x5a);
                     std::string element_tags;
-                    uint32_t tags_bytes = output_element_tags(element_tags,elem.tags);
+                    uint32_t tags_bytes = output_short_array(element_tags,elem.tags);
                     writer.write_varint32(tags_bytes);
                     writer.write_string(element_tags.substr(0,tags_bytes));
                     element_bytes += writer.bytes_written();
                     element_buffer+=tags_buffer.substr(0,writer.bytes_written());
                 }
-/*
+
+                if (elem.index.size() > 1)
                 {
                     //TAG_ELEM_NUM_INDICES
-                    coded_output_->WriteTag(0x08);
-                    coded_output_->WriteVarint32(elem.tags.size());
+                    coded_output_.WriteTag(0x08);
+                    coded_output_.WriteVarint32(elem.index.size());
                 }
-*/
+
                 {
                     // TAG_ELEM_INDEX
                     std::string index_buffer;
                     element_writer writer(index_buffer);
                     writer.write_tag(0x62);
-                    writer.write_varint32(0x01);
-                    writer.write_varint32(elem.path.size());
+                    std::string array_buffer;
+                    uint32_t array_bytes = output_short_array(array_buffer,elem.index);
+                    writer.write_varint32(array_bytes);
+                    writer.write_string(array_buffer.substr(0,array_bytes));
                     element_bytes += writer.bytes_written();
                     element_buffer+=index_buffer.substr(0, writer.bytes_written());
                 }
@@ -299,23 +288,22 @@ public:
                     element_buffer+=coord_buffer.substr(0,writer.bytes_written());
                 }
                 // write element
-                coded_output_->WriteVarint32(element_bytes);
-                coded_output_->WriteRaw(element_buffer.data(), element_bytes);
-
-                std::cerr << "BYTES=" << element_bytes << std::endl;
+                coded_output_.WriteVarint32(element_bytes);
+                coded_output_.WriteRaw(element_buffer.data(), element_bytes);
+                //std::cerr << "BYTES=" << element_bytes << std::endl;
             }
         }
-        return coded_output_->ByteCount();
+        return coded_output_.ByteCount();
     }
 
-    template <typename Tags>
-    uint32_t output_element_tags(std::string & buffer, Tags const& tags)
+    template <typename Array>
+    uint32_t output_short_array(std::string & buffer, Array const& ar)
     {
         element_writer writer(buffer);
-        BOOST_FOREACH(tags::tag_type tag, tags)
+        BOOST_FOREACH(typename Array::value_type elem, ar)
         {
-            std::cerr << tag << ",";
-            writer.write_varint32(tag);
+            //std::cerr << elem << ",";
+            writer.write_varint32(elem);
         }
         return writer.bytes_written();
     }
@@ -326,7 +314,7 @@ public:
         element_writer writer(buffer);
         BOOST_FOREACH(coord_type c, path)
         {
-            std::cerr << c.first << "," << c.second << "\n";
+            //std::cerr << c.first << "," << c.second << "\n";
             writer.write_varint32(c.first);
             writer.write_varint32(c.second);
         }
